f96a9cb3016969d56dc0a940a1ea323de838c9e7
ricardo.ogliari@megaleios.com <rogliariping@gmail.com>
2018-07-16 02:04:17 -0300

    project to arduino uno with rele. Using johnny five library.

---
+++ arduino_uno_johnnyfive.js
@@ -0,0 +1,29 @@
+var five = require("johnny-five"),
+board = new five.Board();
+
+var firebase = require('firebase');
+var config = {
+    apiKey: "AIzaSyBOTL56TVrnSRE-k5ArFdI8AnzT5Pr7jOM",
+    authDomain: "fisl-poa-2018.firebaseapp.com",
+    databaseURL: "https://fisl-poa-2018.firebaseio.com",
+    projectId: "fisl-poa-2018",
+    storageBucket: "fisl-poa-2018.appspot.com",
+    messagingSenderId: "744772420018"    
+};
+firebase.initializeApp(config);
+
+var rootRef = firebase.database().ref("/lamp");
+var name = null;
+
+board.on("ready", function() {
+  var rele = new five.Led(7);
+  
+  rootRef.on('value', function(snapshot) {
+    if (snapshot.val()){
+        rele.on();
+    } else {
+        rele.off();
+    }        
+  });
+
+});
